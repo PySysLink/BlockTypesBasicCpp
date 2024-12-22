035dd67731e414a37f8ed50ffcf7441bfd127691
PelloUsabiaga <pellousabiaga@gmail.com>
2024-12-22 14:24:32 +0100

    Current time added to compute outputs of block

--- src/SimulationBlock.h
+++ src/SimulationBlock.h
@@ -22,7 +22,7 @@ namespace BlockTypes::BasicCpp
         virtual const int GetOutputPortAmmount() const = 0;
         virtual const std::vector<bool> InputsHasDirectFeedthrough() const = 0;
 
-        virtual std::vector<double> CalculateOutputs(const std::vector<double> inputs, std::shared_ptr<SampleTime> sampleTime) = 0;
+        virtual std::vector<double> CalculateOutputs(const std::vector<double> inputs, std::shared_ptr<SampleTime> sampleTime, double currentTime) = 0;
 
         virtual bool TryUpdateConfigurationValue(std::string keyName, ConfigurationValue value) {return false;}
 
